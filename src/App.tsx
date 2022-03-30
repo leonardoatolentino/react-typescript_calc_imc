f475ab49a1434ac0cead0debc7aa0b36f6771822
Leonardo Tolentino <leonardoleoat@yahoo.com.br>
2022-03-29 23:43:31 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import powerImage from './assets/powered.png';
+import styles from './App.module.css';
+import { useState } from 'react';
+const App = () => {
 
-function App() {
+  const [heihtField, setHeihtField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
+  const handleCalculeButton = () => {
+    if(heihtField && weightField){
+
+    }else{
+      alert('Digite todos os campos.');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header className={styles.headerContainer}>
+        <img src={powerImage} alt="" width={200} />
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmentro
+            adorado pela Organização Mudial de Saúde para calcular o peso ideal
+            para cada pessoa.
+          </p>
+
+          <input 
+            type="number"
+            placeholder='Digite sua altura. Ex 1.5 (em métros)'
+            value={heihtField > 0 ? heihtField : ''}
+            onChange={e => setHeihtField(parseFloat(e.target.value))}
+          />
+          <input 
+            type="number"
+            placeholder='Digite seu peso. Ex 75.4 (em kg)'
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculeButton} >Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          ....
+        </div>
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
